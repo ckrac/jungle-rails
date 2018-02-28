2fa2dd8c8c546636e5a94161021a71ae46e1ace6
vagrant <kennethracadio@hotmail.com>
2018-02-28 21:46:12 +0000

    Create login/logout routes and update sessions controller with the logic to log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,10 @@ Rails.application.routes.draw do
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resource :cart, only: [:show] do
     put    :add_item
     delete :remove_item
