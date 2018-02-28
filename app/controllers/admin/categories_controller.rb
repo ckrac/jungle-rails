1bcbf1c74f97316b06024a517fd286cf86c45add
vagrant <kennethracadio@hotmail.com>
2018-02-28 21:15:52 +0000

    Add user model and ran rake db:migrate

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -10,7 +10,7 @@ class Admin::CategoriesController < ApplicationController
 
   def create
     @category = Category.new(category_params)
-
+    # binding.pry
     if @category.save
       redirect_to [:admin, :categories], notice: 'Category created!'
     else
