360a55ce1052563a2764d3666eb3d7bdb6481144
vagrant <kennethracadio@hotmail.com>
2018-03-01 22:52:12 +0000

    Finished step 3

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ActiveRecord::Base
   mount_uploader :image, ProductImageUploader
 
   belongs_to :category
+  belongs_to :review
 
   validates :name, presence: true
   validates :price, presence: true
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  has_many :products
+  has_many :users
+
+  validates :description, presence: true
+  validates :rating, presence: true
 end
