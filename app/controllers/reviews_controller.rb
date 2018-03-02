7350fe97124e0859662101d6e7005b6288c02acf
vagrant <kennethracadio@hotmail.com>
2018-03-02 16:50:43 +0000

    Finished step 11

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,25 @@
 class ReviewsController < ApplicationController
   def create
-    raise "Yay, I'm here!"
+    @review = Review.new(review_params)
+    @product = Product.find params[:product_id]
+
+    @review.product = @product
+    @review.user = current_user
+
+    if @review.save
+      redirect_to :back, notice: 'Review created'
+    else
+      redirect_to :back, notice: 'Review failed to create'
+    end
   end
+
+  private
+    def review_params
+      params.require(:review).permit(
+        :description,
+        :rating
+      )
+    end
 end
+
+
